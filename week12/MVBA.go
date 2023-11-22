d8ee06b58fa1b12ee19d7faed44c1c95aed5a595
Younsur <111101727+Younsur@users.noreply.github.com>
2023-11-22 16:44:32 +0800

    Update MVBA.go

--- week12/MVBA.go
+++ week12/MVBA.go
@@ -1,75 +1,66 @@
-每个节点将自己的输入v_i通过RBC广播出去。
-2. 等待n−f个RBC实例完成后，通过Election()获取当前轮的leader。
-3. 若节点已经完成leader的RBC，则向RABA中输入1。否则，输入0，若随后收到了RBC的结果，则向RABA中重投1。
-4. 若RABA输出1，则将leader的RBC的结果作为输出。否则，重新Election()
-根据这个算法，你应该需要实现RBC(可靠广播协议)并创建多个RBC实例分配给节点，每个节点通过自己的RBC广播消息，还应实现选举机制Election()，选出当前轮的leader，最后还需实现RABA来选择是否将leader的RBC结果作为输出，如不作为，重新选举
-
-type Node struct {  
-    id      int  
-    alive   bool  
-    v       int  
-    output  int  
-    leader  bool  
-    message chan int  
-}  
+package main  
   
-func NewNode(id int, alive bool, v int) *Node {  
-    return &Node{  
-        id:      id,  
-        alive:   alive,  
-        v:       v,  
-        output:  0,  
-        leader:  false,  
-        message: make(chan int),  
+import (  
+    "fmt"  
+    "math/rand"  
+    "sync"  
+    "time"  
+)  
+  
+const (  
+    n     = 5 // 系统中副本的数量  
+    k     = 3 // 系统中正确副本的数量  
+    id    = 0 // 本机的ID  
+    delay = 2 * time.Second // 延迟时间  
+)  
+  
+var wg sync.WaitGroup  
+  
+func main() {  
+    // 初始化本地状态  
+    local := make([]int, n)  
+    for i := 0; i < n; i++ {  
+        local[i] = -1  
     }  
-}  
+    local[id] = 0  
   
-func (n *Node) Broadcast() {  
-    for i := 0; i < 3; i++ { // 假设有3个RBC实例  
-        go func(instance int) {  
-            for {  
-                select {  
-                case <-n.message:  
-                    // 执行RBC操作，此处简化为一直发送消息直到成功  
-                    time.Sleep(time.Millisecond * 100)  
-                    n.message <- n.v  
-                    return  
-                default:  
-                    time.Sleep(time.Millisecond * 10) // 等待一段时间后重试  
-                }  
-            }  
-        }(i)  
+    // 定义回调函数，用于处理接收到的提议  
+    delivered := func(value int) {  
+        if local[value] == -1 {  
+            local[value] = 1  
+        } else {  
+            local[value] = 0  
+        }  
     }  
-}  
   
-func (n *Node) Election() {  
-    for {  
-        select {  
-        case <-n.message:  
-            n.leader = true  
-            n.output = n.v // 将leader的RBC结果作为输出  
-            return  
-        default:  
-            time.Sleep(time.Millisecond * 100) // 等待一段时间后重新选举  
-            if !n.alive {  
-                return  
+    // 定义广播函数，用于将提议广播给其他副本  
+    broadcast := func(value int) {  
+        for i := 0; i < n; i++ {  
+            if i != id {  
+                go func(id int) {  
+                    conn, err := fmt.Dial("tcp", fmt.Sprintf("replica%d:1234", id))  
+                    if err != nil {  
+                        fmt.Println(err)  
+                        return  
+                    }  
+                    defer conn.Close()  
+                    fmt.Fprintf(conn, "delivered:%d\n", value)  
+                }(i)  
             }  
         }  
     }  
-}
-
-func (n *Node) RBC() {  
-    n.Broadcast()  
-    time.Sleep(time.Second * 2) // 等待一段时间以确保RBC操作完成  
-    n.Election()  
-    if n.leader {  
-        outputLock <- true  
-        select {  
-        case outputChan <- n.output: // 将leader的RBC结果发送到外部调用者  
-        default:  
-        }  
-        <-outputLock // 等待外部调用者获取结果后释放锁  
-    } else {  
-        time.Sleep(time.Second * 1) // 等待一段时间后重新尝试Election()  
+  
+    // 进入WRBC阶段，等待其他副本提议值并广播给其他副本自己收到的值  
+    for i := 0; i < n-k+1; i++ {  
+        wg.Add(1)  
+        go func() {  
+            defer wg.Done()  
+            for j := 0; j < k; j++ {  
+                time.Sleep(delay) // 等待一段时间，模拟延迟传输  
+                value := rand.Intn(n) // 生成一个随机值作为提议值  
+                broadcast(value) // 将提议值广播给其他副本  
+                delivered(value) // 处理收到的提议值，更新本地状态  
+            }  
+        }()  
     }  
-}
+    wg.Wait() // 等待所有goroutine执行完毕，确保所有副本都收到的提议值都处理完毕  
